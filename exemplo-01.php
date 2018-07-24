1a6f32cf38b1d9c4166f0eedad17363b432e645c
Gustavo Junqueria <gustavohenriquejunqueira@hotmail.com>
2018-07-24 10:59:28 -0300

    Exemplo -01
    Neste exemplo 01 possui:
    Modo de declaração de variavel;
    Modo de exibir na tela a variavel;
    Modo de verificar qual tipo de variavel é, o tamanho da variavel e o tipo de variavel.

---
+++ exemplo-01.php
@@ -0,0 +1,16 @@
+<?php
+
+$nome = "Gustavo"; /*Tipo basico de variavel string (Palavra) Diferente de outras linguagens de programação, o php não pede que você declare qual o tipo da variavel.*/
+
+echo $nome; /*Forma de exibir na tela a variavel antes declarada, echo no php seria o printf */
+
+
+var_dump($nome); /*VAR_DUMP É uma forma de saber o tipo de dado, o tamanho do dado dentro da variavel */
+ 
+ /*Outro detalhe é que o php quando ele é puro e não vai ter HTML ou css ou java não é necessario declarar o fim de seu codigo. no html devese vehar no final da página o html, já no php não é necessario*/ 
+ 
+
+
+
+
+
